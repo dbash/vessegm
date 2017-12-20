ec4b575340e06ee75fe2e387b0f110bc831e4866
dbash <dbash@bu.edu>
2017-12-20 16:59:41 -0500

    added get_dataset_tuples()

--- img_processing.py
+++ img_processing.py
@@ -9,14 +9,15 @@ from fnmatch import fnmatch
 
 img_path = "/scratch/VESSEL12/images/01/VESSEL12_01.raw"
 mhd_path = "/scratch/VESSEL12/images/01/VESSEL12_01.mhd"
+mhd_mask_path = "/scratch/VESSEL12/masks/VESSEL12_01.mhd"
 idx_slice = 12
 #image = np.empty((512, 512), np.uint16)
-#img = SimpleITK.ReadImage(mhd_path)
+img = SimpleITK.ReadImage(mhd_mask_path)
 #plt.imshow(x)
 #print(img[0:30, 0:30, 12])
 #SimpleITK.Show(img)
 #size = list(img.GetSize())
-#img_arr = SimpleITK.GetArrayFromImage(img)
+img_arr = SimpleITK.GetArrayFromImage(img)
 #print(img_arr.max(), img_arr.min())
 #max, min  = img_arr.max(), img_arr.min()
 #diff = max - min
@@ -25,9 +26,9 @@ idx_slice = 12
 #f, subplot = plt.subplots(2)
 #subplot[0].imshow(img_arr[120], cmap='gray')
 #subplot[1].imshow(img_rescale[120], cmap='gray')
-#plt.imshow(img_arr[120], cmap='gray')
+plt.imshow(img_arr[120], cmap='gray')
 #plt.imshow(img_rescale[120], cmap='gray')
-#plt.show()
+plt.show()
 
 
 def get_image_array(mhd_file, normalize=False):
@@ -73,6 +74,13 @@ def get_dataset(img_folder, mask_folder, normalize=False):
     return img_dict, mask_dict
 
 
+def get_dataset_tuples(img_folder, mask_folder, normalize=False):
+    data = []
+    X, y = get_dataset(img_folder, mask_folder, normalize)
+    for key in X.keys():
+        data.append((X[key], y[key]))
+    del X, y
+    return data
 
 #X, y = get_dataset('/scratch/VESSEL12/images', '/scratch/VESSEL12/masks')
 #print(y['VESSEL12_01.mhd'].shape)
